47332536351c2c848292780e7edf7a15712e4509
MichealLeeCoding <MichealLeeCoding@gmail.com>
2022-11-12 14:22:13 +0700

    set up MVVM structure

---
+++ QuanLyKho/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModels
+
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm ở đây
+        public MainViewModel()
+        {
+            MessageBox.Show("Da vao trong cuar MainViewModel");
+        }
+    }
+}
